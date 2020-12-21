04d7e36254c1e0d3f8dd65298a6b840d303144ff
Nick Nack Attack <nickfarni001@gmail.com>
2020-12-20 21:41:43 -0700

    commit for lesson

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
-import { fetchWithTimeout } from "./services";
-const movies = require("./data/movies.json");
+import {
+    fetchBooks,
+    fetchMovies
+} from "./services";
+
+function getBooksAndMovies() {
+    return Promise.all([fetchBooks(), fetchMovies()])
+        .then(([books, movies ]) => ({
+            books,
+            movies
+        }))
+        .catch(error => console.log("Error fetching books and movies", error));
+}
+
+function getBooksOrMovies() {
+    return Promise.race([fetchBooks(), fetchMovies()])
+        .then(results => results)
+        .catch(err => console.log("Error waiting for the promise race", err))
+}
+
+const getBooksAndMoviesPromise = getBooksAndMovies();
+const getBooksOrMoviesPromise = getBooksOrMovies();
+
+getBooksAndMoviesPromise
+    .then(results => console.log(
+        'getBooksAndMoviesPromise',
+        results
+    ))
+
+getBooksOrMoviesPromise
+    .then(results => console.log(
+        'getBooksOrMoviesPromise',
+        results
+    ))
+
+/*
 
 export function fetchMovies() {
     const resolveFunction = () => movies;
@@ -10,3 +44,5 @@ const moviePromise = fetchMovies();
 moviePromise.then(results => {
     console.log(results);
 });
+
+*/
--- src/services.js
+++ src/services.js
@@ -1,3 +1,17 @@
 export function fetchWithTimeout(delay) {
     return new Promise(resolve => setTimeout(resolve, delay));
 }
+
+export function fetchMovies() {
+    return fetch("./data/movies.json")
+        .then(res => res.json())
+        .then(movies => movies)
+        .catch(err => console.log(err))
+}
+
+export function fetchBooks() {
+    return fetch("./data/books.json")
+        .then(res => res.json())
+        .then(books => books)
+        .catch(err => console.log(err))
+}
