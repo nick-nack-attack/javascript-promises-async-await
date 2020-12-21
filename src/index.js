bc79b53751604e11c31b78f25a2197672ea36fdf
Nick Nack Attack <nickfarni001@gmail.com>
2020-12-20 23:04:20 -0700

    Completed module

--- src/index.js
+++ src/index.js
@@ -45,20 +45,18 @@ async function getBooksAndMoviesAsync() {
         }
     } catch (error) {
         console.log("Error fetching books and movies", error);
+        return error;
     }
 }
 
 async function getBooksOrMoviesAsync() {
-    try {
+
         const values = await Promise.race([
-            asyncFetchBooks(),
+            asyncFetchBookss(),
             asyncFetchMovies()
         ])
         return values;
-    } catch (error) {
-        console.error("Error waiting for the promise race", error);
-        throw error;
-    }
+
 }
 
 getBooksAndMoviesAsync()
