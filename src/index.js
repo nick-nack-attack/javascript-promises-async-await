52ee11f4c41537398479e9093c45167a6dea3639
Nick Nack Attack <nickfarni001@gmail.com>
2020-12-20 22:58:46 -0700

    Completed module

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import {
     asyncFetchBooks,
     asyncFetchMovies
 } from "./services";
-
 const movies = require("./data/movies.json");
 
 const getBooksAndMovies = () => {
@@ -36,10 +35,14 @@ getBooksOrMoviesPromise.then(results => {
 
 async function getBooksAndMoviesAsync() {
     try {
-        const [books, movies] = await Promise.all(
-            ([asyncFetchBooks(), asyncFetchMovies()])
-        )
-        return {books, movies}
+        const [books, movies] = await Promise.all([
+            asyncFetchBooks(),
+            asyncFetchMovies()
+        ])
+        return {
+            books,
+            movies
+        }
     } catch (error) {
         console.log("Error fetching books and movies", error);
     }
@@ -47,21 +50,26 @@ async function getBooksAndMoviesAsync() {
 
 async function getBooksOrMoviesAsync() {
     try {
-        const values = await Promise.race(
-            ([asyncFetchBooks(), asyncFetchMovies()])
-        )
+        const values = await Promise.race([
+            asyncFetchBooks(),
+            asyncFetchMovies()
+        ])
         return values;
     } catch (error) {
         console.error("Error waiting for the promise race", error);
+        throw error;
     }
 }
 
 getBooksAndMoviesAsync()
     .then(results => {
-        console.log("movies and books", {
+        console.log("movies AND books", {
             movies: results.movies,
             books: results.books
         });
+    })
+    .catch(error => {
+        console.error("Error in getBooksAndMoviesAsync execution", error);
     })
 
 getBooksOrMoviesAsync()
@@ -70,3 +78,6 @@ getBooksOrMoviesAsync()
             results
         });
     })
+.catch(error => {
+    console.error("Error in getBooksOrMoviesAsync execution", error);
+});
