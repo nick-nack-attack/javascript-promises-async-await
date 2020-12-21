4f481fbdbd10199bc2a7f69392523933d432aaee
Nick Nack Attack <nickfarni001@gmail.com>
2020-12-20 22:52:01 -0700

    Completed module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import { fetchWithTimeout, fetchMovies, fetchBooks } from "./services";
+import {
+    fetchWithTimeout,
+    fetchMovies,
+    fetchBooks,
+    asyncFetchBooks,
+    asyncFetchMovies
+} from "./services";
+
 const movies = require("./data/movies.json");
 
 const getBooksAndMovies = () => {
@@ -26,3 +33,40 @@ const getBooksOrMoviesPromise = getBooksOrMovies();
 getBooksOrMoviesPromise.then(results => {
     console.log("getBooksOrMoviesPromise", results);
 });
+
+async function getBooksAndMoviesAsync() {
+    try {
+        const [books, movies] = await Promise.all(
+            ([asyncFetchBooks(), asyncFetchMovies()])
+        )
+        return {books, movies}
+    } catch (error) {
+        console.log("Error fetching books and movies", error);
+    }
+}
+
+async function getBooksOrMoviesAsync() {
+    try {
+        const values = await Promise.race(
+            ([asyncFetchBooks(), asyncFetchMovies()])
+        )
+        return values;
+    } catch (error) {
+        console.error("Error waiting for the promise race", error);
+    }
+}
+
+getBooksAndMoviesAsync()
+    .then(results => {
+        console.log("movies and books", {
+            movies: results.movies,
+            books: results.books
+        });
+    })
+
+getBooksOrMoviesAsync()
+    .then(results => {
+        console.log("movies OR books", {
+            results
+        });
+    })
