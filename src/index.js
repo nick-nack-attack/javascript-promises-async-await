19df3e9a348cb20bf03d665407a5475b08911e3b
Nick Nack Attack <nickfarni001@gmail.com>
2020-12-20 21:06:01 -0700

    commit for lesson

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+const movies = require('./data/movies.json');
+const fetchWithTimeout = require('./services');
+
 export const fetchMovies = () => {
 
+    const resolveFunction = () => movies;
+    fetchWithTimeout(1000)
+        .then(resolveFunction())
+
 }
+
+const moviePromise = fetchMovies();
+
+moviePromise.then(results => console.log(results));
